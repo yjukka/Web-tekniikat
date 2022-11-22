5de8feded8acab889c9391fcf461fc9ad297d9e6
yjukka <n2ylju01@students.oamk.fi>
2022-11-22 14:23:10 +0200

    tehtävä 3 tehty

--- WebTekniikat_DomEvents/haku/code.js
+++ WebTekniikat_DomEvents/haku/code.js
@@ -1,7 +1,7 @@
 let btn = document.createElement("button")
 btn.innerText = "Change color!"
 document.body.appendChild(btn)
-let haku = document.querySelector("#search")
+
 function onClicked(){
     document.body.style.backgroundColor = "grey"
 }
@@ -18,7 +18,19 @@ function increaseWidth(){
     buttonStyle.style.width = (currentSize + 50) + "px"
 
 }
+let haku = document.getElementById("search")
+haku.addEventListener("input", searching)
+let listItems = document.querySelectorAll("li")
 function searching(){  
+    let keyWord = haku.value
+    for (const list of listItems) {
+        if (list.textContent.includes(keyWord)){
+            list.style.display = "list-item"
+        }
+        else{
+            list.style.display = "none"
+        }
+    }
     
     
 }
@@ -26,4 +38,3 @@ btn.addEventListener("mouseover", hovered)
 btn.addEventListener("click", onClicked)
 btn.addEventListener("mouseout", hoverOff)
 btn.addEventListener("click", increaseWidth)
-haku.addEventListener("search", searching)
